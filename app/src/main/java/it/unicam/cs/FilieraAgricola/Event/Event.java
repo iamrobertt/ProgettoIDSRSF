2c8888c239b218062af18e2bca556b5ff9b26f37
Franco Acciarresi <francoacciarresi@MacBook-Pro-di-Franco.local>
2025-02-06 19:01:39 +0100

    reviw event, Tastingevent

--- app/src/main/java/it/unicam/cs/FilieraAgricola/Event/Event.java
+++ app/src/main/java/it/unicam/cs/FilieraAgricola/Event/Event.java
@@ -12,7 +12,7 @@ import java.util.List;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "eventID")
+    //@Column(name = "eventID")
     protected int eventID;
 
     protected int eventMaxParticipants;
@@ -22,12 +22,10 @@ import java.util.List;
     protected List<User> participants;
 
     public Event(
-            int eventID,
             int eventMaxParticipants,
             int currentParticipants,
             List<User> participants
     ){
-        this.eventID = eventID;
         this.eventMaxParticipants = eventMaxParticipants;
         this.currentParticipants = currentParticipants;
         this.participants = participants;
@@ -36,13 +34,4 @@ import java.util.List;
     public Event() {
     }
 
-    public int getEventID() {return eventID;}
-    public int getEventMaxParticipants() {return eventMaxParticipants;}
-    public int getCurrentParticipants() {return currentParticipants;}
-    public List<User> getParticipants() {return participants;}
-
-    public void setEventID(int eventID) {this.eventID = eventID;}
-    public void setEventMaxParticipants(int eventMaxParticipants) {this.eventMaxParticipants = eventMaxParticipants;}
-    public void setCurrentParticipants(int currentParticipants) {this.currentParticipants = currentParticipants;}
-    public void setParticipants(List<User> participants) {this.participants = participants;}
 }
