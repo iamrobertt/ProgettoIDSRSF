e578b45a07a4e74670bbaa85f84552b362ae0a82
Franco Acciarresi <francoacciarresi@MacBook-Pro-di-Franco.local>
2025-02-07 11:58:56 +0100

    guestUtility, createEventChechkStreategy, addGuestCheckStrategy

--- app/src/main/java/it/unicam/cs/FilieraAgricola/CheckStrategy/CreateEventCheckStrategy.java
+++ app/src/main/java/it/unicam/cs/FilieraAgricola/CheckStrategy/CreateEventCheckStrategy.java
@@ -1,13 +1,29 @@
 package it.unicam.cs.FilieraAgricola.CheckStrategy;
 
 import it.unicam.cs.FilieraAgricola.Event.Event;
+import it.unicam.cs.FilieraAgricola.Event.EventUtility;
+import it.unicam.cs.FilieraAgricola.Event.GuestUtility;
+import it.unicam.cs.FilieraAgricola.User.User;
+import it.unicam.cs.FilieraAgricola.User.UserUtility;
 
 public class CreateEventCheckStrategy implements CheckStrategy {
-    public CreateEventCheckStrategy(Event event) {
-    }
+
+    private Event event;
+
+    public CreateEventCheckStrategy(Event event) { this.event = event;}
 
     @Override
     public boolean validate() {
+
+        // if the event have not the necessary info
+        if(!EventUtility.checkEventInfo(this.event))
+            return false;
+        // if the event not exist
+        if(!EventUtility.checkExistEvent(this.event))
+            return false;
+
+
+
         return false;
     }
 }
