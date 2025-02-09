d4e9c87989842f6d5336f6a1146481d7959564d4
Shaz Khan <shaxkhax83@gmail.com>
2025-02-09 12:11:50 +0100

    Implementata autenticazione

--- app/src/main/java/it/unicam/cs/FilieraAgricola/CheckStrategy/AuthenticateUserCheckStrategy.java
+++ app/src/main/java/it/unicam/cs/FilieraAgricola/CheckStrategy/AuthenticateUserCheckStrategy.java
@@ -2,15 +2,20 @@ package it.unicam.cs.FilieraAgricola.CheckStrategy;
 
 import it.unicam.cs.FilieraAgricola.User.User;
 import it.unicam.cs.FilieraAgricola.User.UserUtility;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AuthenticateUserCheckStrategy implements CustomCheckStrategy<String, String> {
+public class AuthenticateUserCheckStrategy implements CustomAuthenticateCheckStrategy<String, String> {
 
+    @Autowired
+    private UserUtility userUtility;
 
     @Override
-    public boolean validate(User user , String userEmail, String userPassword) {
-        // TODO : aggiungi getUser(),isAuthenticated()
-        return UserUtility.checkCredentials(userEmail, userPassword);
+    public boolean validate(String userEmail, String userPassword) {
+        User user = this.userUtility.checkCredentials(userEmail, userPassword);
+        if(user == null)
+            return false;
+        return !this.userUtility.isUserAuthenticated(user);
     }
 }
--- app/src/main/java/it/unicam/cs/FilieraAgricola/User/UserUtility.java
+++ app/src/main/java/it/unicam/cs/FilieraAgricola/User/UserUtility.java
@@ -1,19 +1,36 @@
 package it.unicam.cs.FilieraAgricola.User;
 
+
+import it.unicam.cs.FilieraAgricola.Repository.UserRepository;
 import it.unicam.cs.FilieraAgricola.User.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 
+@Service
 public class UserUtility {
+
+    @Autowired
+    private UserRepository userRepository;
 
     public static boolean checkUserInfo(User user) {
         return false;
     }
+
     public static boolean checkExistUser(User user) {
+
         return false;
     }
-    public static boolean checkCredentials(String email, String password) { return false;}
-    //public static User getUser(String email, String password) {return;}
-    public static boolean isUserAuthenticated(User user) { return false;}
+
+    public User checkCredentials(String email, String password) {
+        return this.userRepository.findByUserEmailAndUserPassword(email, password).orElse(null);
+    }
+
+    public boolean isUserAuthenticated(User user) {
+        return user.getUserState().equals(UserState.AUTHENTICATED);
+    }
+
     public static boolean checkExistRole(UserRole role) { return false;}
 
 
