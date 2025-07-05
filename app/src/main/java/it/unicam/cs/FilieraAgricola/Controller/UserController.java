f1e1f2ae6a2f959c48d4da9392cbb0c56eb1cbaf
Shaz Khan <shaxkhan83@gmail.com>
2025-07-05 20:29:59 +0200

    Modificato controllo user exist

--- app/src/main/java/it/unicam/cs/FilieraAgricola/Controller/UserController.java
+++ app/src/main/java/it/unicam/cs/FilieraAgricola/Controller/UserController.java
@@ -53,9 +53,6 @@ public class UserController {
         User user = this.userUtility.getUser(email);
         String jwt;
 
-        if (user == null)
-            throw new IllegalArgumentException("User Not Found");
-
         try {
              jwt = this.userManager.authenticateUserRequest(user, password);
         }
@@ -93,8 +90,7 @@ public class UserController {
         User userValidator = this.userUtility.getUser(userEmail);
 
         User userToValidate = this.userRepository.findById(userID)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
+                .orElse(null);
         try {
             this.userManager.manageUserValidation(userValidator, userToValidate, userValidationState);
         }
@@ -113,8 +109,7 @@ public class UserController {
         User userValidator = this.userUtility.getUser(userEmail);
 
         User userToUpdate = this.userRepository.findById(userID)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
+                .orElse(null);
         try {
             this.userManager.manageRequestRole(userValidator, userToUpdate, userValidationState);
         }
