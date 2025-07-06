487011b67ccc68085afd6c2b05504dfd8ca1617e
Franco Acciarresi <francoacciarresi@MacBook-Pro-di-Franco.local>
2025-07-06 16:17:45 +0200

    creazione classe UserTest, sono stati creati test per la registrazione, per l'autenticazione e per la richiesta di ruolo

---
+++ app/src/Test/it/unicam/cs/FilieraAgricola/User/UserTest.java
@@ -0,0 +1,145 @@
+package it.unicam.cs.FilieraAgricola.User;
+
+import it.unicam.cs.FilieraAgricola.CheckStrategy.AuthenticateUserCheckStrategy;
+import it.unicam.cs.FilieraAgricola.CheckStrategy.RegisterUserCheckStrategy;
+import it.unicam.cs.FilieraAgricola.CheckStrategy.RoleRequestCheckStrategy;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserTest extends User{
+
+    @Autowired
+    private UserUtility userUtility;
+
+    private RegisterUserCheckStrategy registerUserCheckStrategy;
+    private AuthenticateUserCheckStrategy authenticateUserCheckStrategy;
+    private RoleRequestCheckStrategy roleRequestCheckStrategy;
+    private User user;
+
+    public UserTest(
+            long userID,
+            String userName,
+            String userSurname,
+            String userEmail,
+            String userPassword,
+            String companyVatNumber,
+            UserRole userRole,
+            UserState userState
+    ) {
+
+
+    }
+
+    @Test
+    public void registerUser(){
+
+        assertThrows(IllegalArgumentException.class, () -> registerUserCheckStrategy.validate(new User(-1l, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.VALIDATED),new User(1l, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.VALIDATED) ));
+
+        assertThrows(IllegalArgumentException.class, () -> registerUserCheckStrategy.validate(new User(-1, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.VALIDATED),new User(-1, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.VALIDATED)));
+
+        assertThrows(IllegalArgumentException.class, () -> registerUserCheckStrategy.validate(new User(1, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.VALIDATED),new User(1, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.VALIDATED)));
+
+        assertThrows(IllegalArgumentException.class, () -> registerUserCheckStrategy.validate(new User(1L, "","","",
+                "","",UserRole.GENERIC_USER,UserState.VALIDATED),new User(1L, "","","",
+                "","",UserRole.GENERIC_USER,UserState.VALIDATED)));
+
+        assertThrows(IllegalArgumentException.class, () -> registerUserCheckStrategy.validate(new User(1L, "","","",
+                "","",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION),new User(1L, "","","",
+                "","",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION)));
+
+        assertThrows(IllegalArgumentException.class, () -> registerUserCheckStrategy.validate(new User(-1l, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION),new User(1l, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION) ));
+
+        assertThrows(IllegalArgumentException.class, () -> registerUserCheckStrategy.validate(new User(-1, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION),new User(-1, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION)));
+
+        assertThrows(IllegalArgumentException.class, () -> registerUserCheckStrategy.validate(new User(1, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION),new User(1, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION)));
+
+        assertThrows(IllegalArgumentException.class, () -> registerUserCheckStrategy.validate(new User(1L, "","","",
+                "","",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION),new User(1L, "","","",
+                "","",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION)));
+    }
+
+    @Test
+    public void authenticateUser(){
+        assertThrows(IllegalStateException.class, () -> authenticateUserCheckStrategy.validate(new User(-1l, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.VALIDATED), "d"));
+
+        assertThrows(IllegalStateException.class, () -> authenticateUserCheckStrategy.validate(new User(1l, "","","",
+                "d","e",UserRole.GENERIC_USER,UserState.VALIDATED), "d"));
+
+        assertThrows(IllegalStateException.class, () -> authenticateUserCheckStrategy.validate(new User(1l, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.VALIDATED), "e"));
+
+        assertThrows(IllegalStateException.class, () -> authenticateUserCheckStrategy.validate(new User(1l, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.VALIDATED), ""));
+
+        assertThrows(IllegalStateException.class, () -> authenticateUserCheckStrategy.validate(new User(1l, "a","b","c",
+                "","e",UserRole.GENERIC_USER,UserState.VALIDATED), ""));
+
+        assertThrows(IllegalStateException.class, () -> authenticateUserCheckStrategy.validate(new User(1l, "a","b","c",
+                "","e",UserRole.GENERIC_USER,UserState.VALIDATED), "e"));
+
+        assertThrows(IllegalStateException.class, () -> authenticateUserCheckStrategy.validate(new User(-1l, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION), "d"));
+
+        assertThrows(IllegalStateException.class, () -> authenticateUserCheckStrategy.validate(new User(1l, "","","",
+                "d","e",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION), "d"));
+
+        assertThrows(IllegalStateException.class, () -> authenticateUserCheckStrategy.validate(new User(1l, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION), "e"));
+
+        assertThrows(IllegalStateException.class, () -> authenticateUserCheckStrategy.validate(new User(1l, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION), ""));
+
+        assertThrows(IllegalStateException.class, () -> authenticateUserCheckStrategy.validate(new User(1l, "a","b","c",
+                "","e",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION), ""));
+
+        assertThrows(IllegalStateException.class, () -> authenticateUserCheckStrategy.validate(new User(1l, "a","b","c",
+                "","e",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION), "e"));
+    }
+
+    @Test
+    public void userRequestRole(){
+
+        assertThrows(IllegalStateException.class, () -> roleRequestCheckStrategy.validate(new User(-1l, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.VALIDATED), UserRole.GENERIC_USER));
+
+        assertThrows(IllegalStateException.class, () -> roleRequestCheckStrategy.validate(new User(1l, "","","",
+                "","",UserRole.GENERIC_USER,UserState.VALIDATED), UserRole.GENERIC_USER));
+
+        assertThrows(IllegalStateException.class, () -> roleRequestCheckStrategy.validate(new User(-1l, "a","b","c",
+                "d","e",null,UserState.VALIDATED), UserRole.GENERIC_USER));
+
+        assertThrows(IllegalStateException.class, () -> roleRequestCheckStrategy.validate(new User(1l, "","","",
+                "","",null,UserState.VALIDATED), UserRole.GENERIC_USER));
+
+        assertThrows(IllegalStateException.class, () -> roleRequestCheckStrategy.validate(new User(-1l, "a","b","c",
+                "d","e",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION), UserRole.GENERIC_USER));
+
+        assertThrows(IllegalStateException.class, () -> roleRequestCheckStrategy.validate(new User(1l, "","","",
+                "","",UserRole.GENERIC_USER,UserState.WAITING_FOR_VALIDATION), UserRole.GENERIC_USER));
+
+        assertThrows(IllegalStateException.class, () -> roleRequestCheckStrategy.validate(new User(-1l, "a","b","c",
+                "d","e",null,UserState.WAITING_FOR_VALIDATION), UserRole.GENERIC_USER));
+
+        assertThrows(IllegalStateException.class, () -> roleRequestCheckStrategy.validate(new User(1l, "","","",
+                "","",null,UserState.WAITING_FOR_VALIDATION), UserRole.GENERIC_USER));
+    }
+
+
+
+
+}
