b40b5185278ef7b737ebab69489f0658e31ab8bc
francoacciarresi <franco.acciarresi@studenti.unicam.it>
2025-07-12 17:36:34 +0200

    sistemata la classe test di productTest parte 2

--- app/src/Test/java/it/unicam/cs/FilieraAgricola/Test/Product/ProductTest.java
+++ app/src/Test/java/it/unicam/cs/FilieraAgricola/Test/Product/ProductTest.java
@@ -8,6 +8,7 @@ import it.unicam.cs.FilieraAgricola.Product.*;
 import it.unicam.cs.FilieraAgricola.User.User;
 import it.unicam.cs.FilieraAgricola.User.UserRole;
 import it.unicam.cs.FilieraAgricola.User.UserState;
+import it.unicam.cs.FilieraAgricola.User.UserUtility;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,6 +21,8 @@ public class ProductTest extends Product {
 
     @Autowired
     private ProductUtility productUtility;
+    @Autowired
+    private UserUtility userUtility;
 
     private ValidateProductCheckStrategy validateProductCheckStrategy;
     private SellProductCheckStrategy sellProductCheckStrategy;
@@ -209,12 +212,135 @@ public class ProductTest extends Product {
 
     @Test
     public void validateBoughtProduct() {
-        Product product2 = new SingleProduct();
+        Product  product = new SingleProduct(66,"Anatra5","Pera Cotogna",12.3,5,ProductState.PRODUCT_TO_VALIDATE,ProductType.SINGLE);
+        User user = new User(2,"Franco","Accia","prova2@prova.prova","juventus12345","1234567", UserRole.CUSTOMER,UserState.VALIDATED);
+
+        // mi aspetto un errore poichè prodotto è nullo
+        assertThrows(IllegalArgumentException.class, ()->buyProductCheckStrategy.validate(user,null,2));
+
+        //mi aspetto un errore poichè user è null
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(null,product,2));
+
+        //mi aspetto un errore poichè la quantità è < 1
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,0));
+
+        //mi aspetto che mi dia errore poichè utente non ha il ruolo per comprare
+        user.setUserRole(UserRole.GENERIC_USER);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+        user.setUserRole(UserRole.SELLER);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+        user.setUserRole(UserRole.VALIDATOR);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+        user.setUserRole(UserRole.PROMOTER);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+        user.setUserRole(UserRole.PRODUCER);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+        user.setUserRole(UserRole.ADMINISTRATOR);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+        user.setUserRole(UserRole.MANUFACTURER);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+        user.setUserRole(UserRole.DISTRIBUTOR);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+
+        user.setUserRole(UserRole.CUSTOMER);
+
+        //mi aspetto che mi dia errore poichè lo stato del prodotto non è corretto
+        product.setProductState(ProductState.PRODUCT_TO_VALIDATE);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+        product.setProductState(ProductState.PRODUCT_INSERTED);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+        product.setProductState(ProductState.PRODUCT_NOT_VALIDATED);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+
+        product.setProductState(ProductState.PRODUCT_VALIDATED);
+
+        //mi aspetto che mi dia errore poichè la quantià richiesta è maggiore rispetto a quella reale
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,6));
+
+        // mi aspetto che mi dia errore poichè l'uetnete non esiste
+        user.setUserID(-1);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+
+        user.setUserID(2);
+
+        //mi aspetto che mi da errore poichè il prodotto non esiste
+        product.setProductID(-1);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+
+        // mi aspetto che mi dia errore poichè user e product non esistono
+        user.setUserID(-1);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+
+        user.setUserID(2);
+        product.setProductID(66);
+
+        // mi aspetto un errore poichè mancano le info dell'utente
+        user.setUserName(null);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+
+        // mi aspetto un errore poichè mancano le info su user e product
+        product.setProductName(null);
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
+
+        //mi aspetto un errore poichè le info del prodotto sono errate
+        user.setUserName("Franco");
+        assertThrows(IllegalStateException.class, ()->buyProductCheckStrategy.validate(user,product,2));
 
     }
 
-    @Test public void valodateLoadProduct(){
-        Product product1 = new SingleProduct();
+    @Test public void validateLoadProduct(){
+        Product  product = new SingleProduct(66,"Anatra5","Pera Cotogna",12.3,5,ProductState.PRODUCT_TO_VALIDATE,ProductType.SINGLE);
+        User user = new User(2,"Franco","Accia","prova2@prova.prova","juventus12345","1234567", UserRole.SELLER,UserState.VALIDATED);
+
+        //mi aspetto un errore, utente non valido
+        assertThrows(IllegalStateException.class, ()-> loadProductCheckStrategy.validate(null,product));
+
+        //mi aspetto un errore, prodotto non valido
+        assertThrows(IllegalStateException.class, ()-> loadProductCheckStrategy.validate(user,null));
+
+
+        // Mi aspetto un errore perchè la quantità in magazzino è inferiore a quella richiesta
+        WarehouseProduct whereasProduct = new WarehouseProduct(product,4);
+        assertThrows(IllegalArgumentException.class, () -> loadProductCheckStrategy.validate(user, whereasProduct.getProduct()));
+
+        //mi aspetto un errore perchè la quantità in magazzino è < 1
+        whereasProduct.setProductQuantity(0);
+        assertThrows(IllegalArgumentException.class, ()->loadProductCheckStrategy.validate(user, whereasProduct.getProduct()));
+
+        whereasProduct.setProductQuantity(5);
+
+        //MI aspetto un errore perchè il ruolo
+        user.setUserRole(UserRole.CUSTOMER);
+        assertThrows(IllegalStateException.class, ()->loadProductCheckStrategy.validate(user,product));
+        user.setUserRole(UserRole.VALIDATOR);
+        assertThrows(IllegalStateException.class, ()->loadProductCheckStrategy.validate(user,product));
+        user.setUserRole(UserRole.PROMOTER);
+        assertThrows(IllegalStateException.class, ()->loadProductCheckStrategy.validate(user,product));
+        user.setUserRole(UserRole.GENERIC_USER);
+        assertThrows(IllegalStateException.class, ()->loadProductCheckStrategy.validate(user,product));
+        user.setUserRole(UserRole.PRODUCER);
+        assertThrows(IllegalStateException.class, ()->loadProductCheckStrategy.validate(user,product));
+        user.setUserRole(UserRole.DISTRIBUTOR);
+        assertThrows(IllegalStateException.class, ()->loadProductCheckStrategy.validate(user,product));
+        user.setUserRole(UserRole.MANUFACTURER);
+        assertThrows(IllegalStateException.class, ()->loadProductCheckStrategy.validate(user,product));
+        user.setUserRole(UserRole.ADMINISTRATOR);
+        assertThrows(IllegalStateException.class, ()->loadProductCheckStrategy.validate(user,product));
+
+        // mi aspetto che mi dia errore poichè le info del prodotto sono nulle
+        user.setUserSurname(null);
+        assertThrows(IllegalStateException.class, ()-> userUtility.checkUserInfo(user));
+
+        user.setUserSurname("Accia");
+
+        //mi aspetto che mi dia errore poichè le info sono errate
+        product.setProductDescription(null);
+        assertThrows(IllegalStateException.class, ()->productUtility.checkProductInfo(product));
+
+        product.setProductDescription("pera Cotogna");
+
+        //mi aspetto che mi dia errore poichè sono stati gia caricati
+        assertThrows(IllegalStateException.class, ()-> productUtility.checkExistProductWithUser(user,product));
 
     }
 
